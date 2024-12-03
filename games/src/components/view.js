e36eacb65cb3e840822ce50ba9b730d56803024f
Brian Walsh <G00424410@atu.ie>
2024-12-03 20:06:06 +0000

    Styling changes to view.js. Background image

--- games/src/components/view.js
+++ games/src/components/view.js
@@ -20,13 +20,24 @@ function View() {
     Reload();
   }, []);
 
+  const centerStyle = {
+    textAlign: 'center', // Centers text horizontally
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center', // Centers items horizontally
+    justifyContent: 'center', // Centers items vertically
+    minHeight: '100vh', // Ensures full-page centering
+    backgroundImage: 'url("https://www.pcworld.com/wp-content/uploads/2024/05/steam-game-library.jpg?quality=50&strip=all")',
+    
+  };
+
   return (
-    <div>
-      <h1>Game List</h1>
+    <div style={centerStyle}>
+      <h1 style={{ textAlign: 'center', color: '#222', backgroundColor: 'black', color: '#bd0000', fontFamily: 'Nosifer', }}>Game List</h1>
       {/* Pass the game data to the Games component */}
       <Games myGames={data} ReloadData={Reload} />
     </div>
   );
-};
+}
 
 export default View;
